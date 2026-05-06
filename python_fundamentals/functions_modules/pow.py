ae78a63236b89d279f59e0ea6f76555720fd4388
Litoxam <litobernier@gmail.com>
2026-05-06 22:09:41 +0200

    fix: removed the prints

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -4,9 +4,9 @@ def pow(a, b):
     """return the value of a raised to the power of b"""
     if b == 0:
         return 1
-    
+
     power = 1
     for i in range(b):
         power *= a
-    
+
     return power
