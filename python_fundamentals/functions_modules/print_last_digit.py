cd46692a1224d772cd8a318aa17323670382224e
Litoxam <litobernier@gmail.com>
2026-05-06 21:45:23 +0200

    fix: use of the abs() function

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -2,8 +2,7 @@
 
 def print_last_digit(number):
     """Print the last digit of number"""
-    if number < 0:
-        last_digit = -(number % -10)
-    else:
-        last_digit = number % 10
-    print(last_digit)
+    last_digit = abs(number) % 10
+    print("{}".format(last_digit), end="")
+    
+    return (last_digit)
