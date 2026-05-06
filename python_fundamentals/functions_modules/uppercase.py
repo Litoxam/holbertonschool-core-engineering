408caffe84ee6779460fe4e026b5a32332c9605e
Litoxam <litobernier@gmail.com>
2026-05-06 10:30:29 +0200

    fix: deleted the comment

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -3,7 +3,7 @@
 def uppercase(str):
     """ print the string in uppercase followed by a new line"""
     for char in str:
-        ascii_value = ord(char)  # Get the ascii value of each char
+        ascii_value = ord(char)
 
         if 97 <= ascii_value <= 122:
             print("{}".format(chr(ascii_value - 32)), end="")
