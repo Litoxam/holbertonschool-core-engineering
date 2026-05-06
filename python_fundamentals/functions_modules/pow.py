5f869f09ff15abca907085dc5f6256d91ac658c0
Litoxam <litobernier@gmail.com>
2026-05-06 22:21:43 +0200

    fix: new variable exponent which stock the absolute value of b

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -2,15 +2,15 @@
 
 def pow(a, b):
     """return the value of a raised to the power of b"""
+    power = 1
+    exponent = abs(b)
     if b == 0:
         return 1
 
     if b < 0:
         a = 1 / a
-        b = -b
 
-    power = 1
-    for i in range(b):
+    for i in range(exponent):
         power *= a
 
     return power
