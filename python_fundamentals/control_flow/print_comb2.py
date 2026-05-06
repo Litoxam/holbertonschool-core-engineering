a3fd73b32280e41e9e41f7f7f5dff6c006242e64
Litoxam <litobernier@gmail.com>
2026-05-06 09:55:20 +0200

    fix: double quotes, and deleted an extra line

--- python_fundamentals/control_flow/print_comb2.py
+++ python_fundamentals/control_flow/print_comb2.py
@@ -2,9 +2,8 @@
 
 for i in range(100):
     if i < 99:
-        print('{:02d}'.format(i), end=', ')
+        print("{:02d}".format(i), end=", ")
     else:
-        print('{:02d}'.format(i))
-
+        print("{:02d}".format(i))
 #  0 will fill the blank spaces, 2 is the width of our number
 #  d is for the integer format
