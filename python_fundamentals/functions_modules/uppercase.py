a121e328216bcf76c5671ff94a7f8ce67d6579a3
Litoxam <litobernier@gmail.com>
2026-05-06 11:29:07 +0200

    fix: deleted extra space in the description

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def uppercase(str):
-    """ print the string in uppercase followed by a new line"""
+    """print the string in uppercase followed by a new line"""
     result = ""
     for char in str:
         if ord('a') <= ord(char) <= ord('z'):
