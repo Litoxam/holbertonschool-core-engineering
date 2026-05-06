c0c3e762e99be773abbb5011f8f4a2c62f58d3a8
Litoxam <litobernier@gmail.com>
2026-05-06 10:32:09 +0200

    fix: changed char to i, and deleted extra line

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -2,11 +2,10 @@
 
 def uppercase(str):
     """ print the string in uppercase followed by a new line"""
-    for char in str:
-        ascii_value = ord(char)
-
+    for i in str:
+        ascii_value = ord(i)
         if 97 <= ascii_value <= 122:
             print("{}".format(chr(ascii_value - 32)), end="")
         else:
-            print("{}".format(char), end="")
+            print("{}".format(i), end="")
     print("")
