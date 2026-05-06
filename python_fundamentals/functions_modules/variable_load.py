e34c87b86ad798950a91d9246dd24f353fc68278
Litoxam <litobernier@gmail.com>
2026-05-06 23:39:33 +0200

    fix: changed the name of the function. Imported a from variable_load_5

--- python_fundamentals/functions_modules/variable_load.py
+++ python_fundamentals/functions_modules/variable_load.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-import variable_load_5
+from variable_load_5 import a
 
 
-def variable():
-    print("{}".format(variable_load_5.a))
+def print_variable():
+    print("{}".format(a))
 
 
 if __name__ == "__main__":
-    variable()
+    print_variable()
