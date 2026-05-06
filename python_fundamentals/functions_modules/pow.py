e4eace84e1e7e49f42fa9bf65c0891d24ba2c867
Litoxam <litobernier@gmail.com>
2026-05-06 22:06:23 +0200

    fix: added the print and the return

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -3,11 +3,11 @@
 def pow(a, b):
     """return the value of a raised to the power of b"""
     if b == 0:
+        print(1)
         return 1
     
-    power = a
-    for i in range(b - 1):
+    power = 1
+    for i in range(b):
         power *= a
     print("{}".format(power))
-
-    return (power)
+    return power
