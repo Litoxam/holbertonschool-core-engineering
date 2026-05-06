2afa75f4e1fd6dbe611ee9d0756e2bbe209ba40a
Litoxam <litobernier@gmail.com>
2026-05-06 10:03:09 +0200

    changed the logic of the code

--- python_fundamentals/control_flow/print_comb2.py
+++ python_fundamentals/control_flow/print_comb2.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 
 for i in range(100):
-    if i < 99:
-        print("{:02d}".format(i), end=", ")
+    if i == 99:
+        print("{:02d}".format(i), end="")
     else:
-        print("{:02d}".format(i))
-#  0 will fill the blank spaces, 2 is the width of our number
-#  d is for the integer format
-
+        print("{:02d}".format(i), end=", ")
