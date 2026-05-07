387288e4282c2a541fadb96b71190b7eeb0b7a0b
Litoxam <litobernier@gmail.com>
2026-05-07 21:23:37 +0200

    feat: prints a matrix of integers

---
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    """prints a matrix of integers"""
+    for row in matrix:
+        for i in range(len(row)):
+            print("{:d} ".format(row[i]), end="")
+        print()
