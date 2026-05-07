8645bf9620c9b213a2ff9b0b4769b5af3a814725
Litoxam <litobernier@gmail.com>
2026-05-08 01:09:42 +0200

    fix: use of the method max, with the syntaxe a_dictionary.get

--- python_fundamentals/core_data_structures/best_score.py
+++ python_fundamentals/core_data_structures/best_score.py
@@ -5,5 +5,5 @@ def best_score(a_dictionary):
     """returns the key with the biggest integer value"""
     if not a_dictionary:
         return None
-    best_score = max(a_dictionary)
+    best_score = max(a_dictionary, key=a_dictionary.get)
     return best_score
