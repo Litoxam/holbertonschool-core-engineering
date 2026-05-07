c8ea43334f994c9985bc1413d2becb86b2c1e3dd
Litoxam <litobernier@gmail.com>
2026-05-07 21:26:09 +0200

    fix: handle spaces betwen values and remove the space after final value

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -5,5 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     """prints a matrix of integers"""
     for row in matrix:
         for i in range(len(row)):
-            print("{:d} ".format(row[i]), end="")
+            print("{:d}".format(row[i]), end="")
+            if i < len(row) - 1:
+
         print()
