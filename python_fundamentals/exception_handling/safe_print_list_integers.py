6f0fc9a85d15a22a9fe77d2cbc39349468f5f3de
Litoxam <litobernier@gmail.com>
2026-05-11 16:10:55 +0200

    prints the first x elements of a list. Handle if index is out of range or if Type/ValueError

---
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    """prints the first x elements of a list"""
+    elements = 0
+    for y in range(x):
+        try:
+            print("{:d}".format(my_list[y]), end="")
+            elements += 1
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
+            break
+    print()
+    return elements
