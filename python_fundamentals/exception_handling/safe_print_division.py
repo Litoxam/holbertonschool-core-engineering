46c739e0dc9d57c3ed0ad9768c55ced8ed1d9493
Litoxam <litobernier@gmail.com>
2026-05-11 16:47:50 +0200

    Divides two integers, safe version with try/except/finally

---
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def safe_print_division(a, b):
+    """divides two integers"""
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
