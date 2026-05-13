e6338fa293599bd42ba8125374519a6fcdcd0897
Litoxam <litobernier@gmail.com>
2026-05-13 15:36:38 +0200

    Define a class Square with a private instance attribute 'size'

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Introduce instance attributes."""
+
+
+class Square:
+    """a private instance attribute size"""
+    def __init__(self, size):
+        self.__size = size
