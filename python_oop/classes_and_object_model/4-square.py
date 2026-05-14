5f8a45f16de5d23d2dce66c743a4e4d3861c53c6
Litoxam <litobernier@gmail.com>
2026-05-14 20:18:00 +0200

    fix: add a property(get_size, set_size)

--- python_oop/classes_and_object_model/4-square.py
+++ python_oop/classes_and_object_model/4-square.py
@@ -11,18 +11,18 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-    def area(self):
-        """return the area of square"""
-        return self.__size * self.__size
-
     def get_size(self):
         """get the value of __size"""
         return self.__size
 
-    def set_size(self, size):
-        """set the private __size attribute"""
-        if not isinstance(size, int):
+    def set_size(self, new_size):
+        if not isinstance(new_size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if new_size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = new_size
+
+    def area(self):
+        return self.__size * self.__size
+
+    size = property(get_size, set_size)
