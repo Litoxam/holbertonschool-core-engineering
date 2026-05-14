2edee4a90c4d7032b26936d93e8f32df9282a7c2
Litoxam <litobernier@gmail.com>
2026-05-14 17:18:32 +0200

    doc: Added docstrings

--- python_oop/classes_and_object_model/4-square.py
+++ python_oop/classes_and_object_model/4-square.py
@@ -12,12 +12,15 @@ class Square:
         self.__size = size
 
     def area(self):
+        """return the area of square"""
         return self.__size * self.__size
 
     def get_size(self):
+        """get the value of __size"""
         return self.__size
 
     def set_size(self, size):
+        """set the private __size attribute"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
