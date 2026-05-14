de6aaa9c104e7957c3b9c1f9518489650e254e40
Litoxam <litobernier@gmail.com>
2026-05-14 21:47:01 +0200

    Added instances area and perimeter

---
+++ python_oop/classes_and_object_model/2-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Reinforce object modeling with a second class."""
+
+
+class Rectangle:
+    """Define a class Rectangle."""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def get_width(self):
+        """returns the width"""
+        return self.__width
+
+    def set_width(self, value):
+        """set new value to width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def get_height(self):
+        """returns height"""
+        return self.__height
+
+    def set_height(self, value):
+        """set new value to height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.height == 0:
+            return 0
+        return (self.__height * 2) + (self.__width * 2)
+
+    width = property(get_width, set_width)
+    height = property(get_height, set_height)
