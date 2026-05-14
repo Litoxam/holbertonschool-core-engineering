1ebc69b9e43baed30f89cf4158c496e90bb57110
Litoxam <litobernier@gmail.com>
2026-05-14 22:29:19 +0200

    fix: condition if tuple is not full of ints, or ints are negative

--- python_oop/classes_and_object_model/6-square.py
+++ python_oop/classes_and_object_model/6-square.py
@@ -38,6 +38,9 @@ class Square:
     def set_position(self, value):
         if (not isinstance(value, tuple) or len(value) != 2):
             raise TypeError("position must be a tuple of 2 positive integers")
+        for i in value:
+            if (not isinstance(i, int) or (i < 0)):
+                raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     size = property(get_size, set_size)
