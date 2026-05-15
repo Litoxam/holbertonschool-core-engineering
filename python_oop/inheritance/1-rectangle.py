06b1bd071634dcd8268eb425b955472f9210deb8
Litoxam <litobernier@gmail.com>
2026-05-15 12:25:23 +0200

    fix: integer_validator raise an error if value is boolean

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -8,18 +8,18 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
+        if (not isinstance(value, int) or
+                type(value) is bool):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        return value
 
 
 class Rectangle(BaseGeometry):
     """Create a class named Rectangle, inherits from BaseGeometry
     Both values are validated by the integer_validator
     """
-    def __init__(self, width, height):
+    def __init__(self, width=0, height=0):
 
         self.integer_validator("width", width)
         self.integer_validator("height", height)
