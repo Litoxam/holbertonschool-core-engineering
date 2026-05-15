1b478f22de79732b69c2f851b3a5248165b818da
Litoxam <litobernier@gmail.com>
2026-05-15 13:01:35 +0200

    fix: changed the file imported

--- python_oop/inheritance/1-square.py
+++ python_oop/inheritance/1-square.py
@@ -2,22 +2,18 @@
 """Introduction to Inheritance and Polymorphism"""
 
 
-Rectangle = __import__('1-rectangle').Rectangle
+Rectangle = __import__('2-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """Create a class named Square, inherits from Rectangle"""
     def __init__(self, size):
         """Init Square, validate size"""
+        self.integer_validator("size", size)
         Rectangle.__init__(self, size, size)
         # or super().__init__(size, size)
-        self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
         """Return the area of the Square"""
         return self.__size * self.__size
-
-    def __str__(self):
-        """Return the print() and str() representation of the Square"""
-        return "[Square] {}/{}".format(self.__size, self.__size)
