07804b594e62df2d048048b93bbff5f7e0f55bd7
Litoxam <litobernier@gmail.com>
2026-05-15 12:12:31 +0200

    feat: created a classe Rectangle inheriting from BaseGeomtry. Values are checked with integer_validator

---
+++ python_oop/inheritance/1-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Introduction to Inheritance and Polymorphism"""
+
+
+class BaseGeometry:
+    """Create a class named BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        return value
+
+
+class Rectangle(BaseGeometry):
+    """Create a class named Rectangle, inherits from BaseGeometry
+    Both values are validated by the integer_validator
+    """
+    def __init__(self, width, height):
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
