d38ee0c16217a963c7bc82267ba0920d23033317
Litoxam <litobernier@gmail.com>
2026-05-15 14:17:23 +0200

    feat: created a class Fish and its children Bird and Fish. class FlyingFish inherits from both subclasses

---
+++ python_oop/abc/flyingfish.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+""""""
+
+from abc import ABC, abstractmethod
+import math
+
+
+class Fish():
+    """class named FlyingFish"""
+    def swim(self):
+        """Method swim that prints The fish is swimming"""
+        print("The fish is swimming")
+
+    def habitat(self):
+        """Methode habitat that prints The fish lives in water"""
+        print("The fish lives in water")
+
+
+class Bird():
+    """class named Bird"""
+    def fly(self):
+        """Method that prints The bird is flying"""
+        print("The bird is flying")
+
+    def habitat(self):
+        """Method habitat that prints The bird lives the sky"""
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """class named FlyingFishBird that inherits from FlyingFish and Bird"""
+
+    def fly(self):
+        """Method fly that prints The flying fish is soaring"""
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        """Method swim that prints The flying fish is swimming"""
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        """Method habitat"""
+        print("The flying fish lives both in water and the sky")
+
+
+if __name__ == "__main__":
+    flying_fish = FlyingFish()
+    flying_fish.swim()
+    flying_fish.fly()
+    flying_fish.habitat()
+    print(flying_fish.__class__.__mro__)
