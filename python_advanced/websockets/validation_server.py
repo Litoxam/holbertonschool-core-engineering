126eddc3cc165378d1c22eb0ac69efec18b06eb3
Litoxam <litobernier@gmail.com>
2026-07-13 16:14:08 +0200

    feat: validate the string before printing

---
+++ python_advanced/websockets/validation_server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import asyncio
+from websockets.asyncio.server import serve
+
+
+# Handle each client connection.
+async def connection_handler(websocket):
+    async for message in websocket:
+        # Message printed if not empty
+        if len(message.strip()) > 0:
+            print(f"Received: {message}")
+        else:
+            print("ERR:EMPTY")
+
+        await websocket.send(message)
+        # Print a message when the client is disconnected
+    print("Client disconnected")
+
+
+async def main():
+    # Start the WebSocket server on localhost:8765.
+    async with serve(connection_handler, "localhost", 8765) as ws:
+        print("Server is running on ws://localhost:8765")
+        # Keep the server running forever.
+        await ws.serve_forever()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
