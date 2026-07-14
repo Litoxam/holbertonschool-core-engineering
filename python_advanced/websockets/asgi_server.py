2d8ba42dbd5c2b9279f1e928b9a8ee95329d9a72
Litoxam <litobernier@gmail.com>
2026-07-14 16:06:06 +0200

    feat: starlette application with http and websocket endpoint

--- python_advanced/websockets/asgi_server.py
+++ python_advanced/websockets/asgi_server.py
@@ -2,17 +2,20 @@ from starlette.applications import Starlette
 from starlette.responses import HTMLResponse
 from starlette.routing import Route, WebSocketRoute
 
+
 async def homepage(request):
     print("Hello")
     return HTMLResponse("<h1>WebSocket App</h1>")
+
 
 async def websocket_endpoint(websocket):
     await websocket.accept()
 
     while True:
-            message = await websocket.receive_text()
-            # send back the message the client sent
-            await websocket.send_text(message)
+        message = await websocket.receive_text()
+        # send back the message the client sent
+        await websocket.send_text(message)
+
 
 app = Starlette(routes=[
     Route("/", homepage),
